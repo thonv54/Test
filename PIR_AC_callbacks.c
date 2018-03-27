5dee8fea4b679bba31198feeffcf91b74ede2b32
thonv <thonv54@gmail.com>
2018-03-27 17:45:42 +0700

    first Commit

---
+++ PIR_AC_callbacks.c
@@ -0,0 +1,68 @@
+// This callback file is created for your convenience. You may add application
+// code to this file. If you regenerate this file over a previous version, the
+// previous version will be overwritten and any code you have added will be
+// lost.
+
+#include "app/framework/include/af.h"
+
+
+/** @brief Main Init
+ *
+ * This function is called from the application's main function. It gives the
+ * application a chance to do any initialization required at system startup. Any
+ * code that you would normally put into the top of the application's main()
+ * routine should be put into this function. This is called before the clusters,
+ * plugins, and the network are initialized so some functionality is not yet
+ * available.
+        Note: No callback in the Application Framework is
+ * associated with resource cleanup. If you are implementing your application on
+ * a Unix host where resource cleanup is a consideration, we expect that you
+ * will use the standard Posix system calls, including the use of atexit() and
+ * handlers for signals such as SIGTERM, SIGINT, SIGCHLD, SIGPIPE and so on. If
+ * you use the signal() function to register your signal handler, please mind
+ * the returned value which may be an Application Framework function. If the
+ * return value is non-null, please make sure that you call the returned
+ * function from your handler to avoid negating the resource cleanup of the
+ * Application Framework itself.
+ *
+ */
+void emberAfMainInitCallback(void)
+{
+}
+
+/** @brief Pre ZDO Message Received
+ *
+ * This function passes the application an incoming ZDO message and gives the
+ * appictation the opportunity to handle it. By default, this callback returns
+ * false indicating that the incoming ZDO message has not been handled and
+ * should be handled by the Application Framework.
+ *
+ * @param emberNodeId   Ver.: always
+ * @param apsFrame   Ver.: always
+ * @param message   Ver.: always
+ * @param length   Ver.: always
+ */
+boolean emberAfPreZDOMessageReceivedCallback(EmberNodeId emberNodeId,
+                                             EmberApsFrame* apsFrame,
+                                             int8u* message,
+                                             int16u length)
+{
+  return false;
+}
+
+/** @brief Stack Status
+ *
+ * This function is called by the application framework from the stack status
+ * handler.  This callbacks provides applications an opportunity to be notified
+ * of changes to the stack status and take appropriate action.  The return code
+ * from this callback is ignored by the framework.  The framework will always
+ * process the stack status after the callback returns.
+ *
+ * @param status   Ver.: always
+ */
+boolean emberAfStackStatusCallback(EmberStatus status)
+{
+  return false;
+}
+
+
